7f5b32ed77ff9873866d4d10469fd49ea8471aae
nurbek <nashirbekov35@gmail.com>
2021-02-11 17:25:31 +0600

    авторизация

--- movie_dj/settings.py
+++ movie_dj/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
 
     'snowpenguin.django.recaptcha3',
 
+    'allauth',
+    'allauth.account',
+
 ]
 
 MIDDLEWARE = [
@@ -94,6 +97,10 @@ DATABASES = {
 }
 
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -125,6 +132,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 
 # Static files (CSS, JavaScript, Images)
--- movies/urls.py
+++ movies/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MoviesView.as_view()),
+    path("", views.MoviesView.as_view(), name="main"),
     path("filter/", views.FilterMoviesView.as_view(), name="filter"),
     path("search/", views.Search.as_view(), name="search"),
     path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
